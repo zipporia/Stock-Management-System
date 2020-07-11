13613af2c8f3ed9600fb0bdf2462a09aa9607843
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-11 21:54:52 +0800

    login fixed

--- index.php
+++ index.php
@@ -1,5 +1,48 @@
 <?php
     require_once 'php_action/db_connect.php';
+
+    session_start();
+
+    $errors = array();
+
+    if($_POST){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if(empty($username) || empty($password)){
+            if($username == ""){
+                $errors[] = "Username is required";
+            }
+
+            if($password == ""){
+                $errors[] = "Password is required";
+            }
+        }else{
+            $sql = "SELECT * FROM users WHERE user_name = '$username'";
+            $result = $conn->query($sql);
+
+            if($result->num_rows == 1){
+                $password = md5($password);
+                //exists
+                $mainSql = "SELECT * FROM users WHERE user_name = '$username' AND user_pwd = '$password'";
+                $mainResult = $conn->query($mainSql);
+
+                if($mainResult->num_rows == 1){
+                    $value = $mainResult->fetch_assoc();
+                    $user_id = $value['user_id'];
+
+                    //set session
+                    $_SESSION['userId'] = $user_id;
+
+                    header('Location: dashboard.php');
+                }else{
+                    $errors[] = "Incorrect username/password combination";
+                }
+            }else{
+                $errors[] = "Username does not exists";
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -26,9 +69,46 @@
 <body>
     
     <div class="container">
-        <div class="row">
-            <div class="col-md-5">
-                Keep Grinding
+        <div class="row vertical">
+            <div class="col-md-5 col-md-offset-4">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Please Sign In</h3>
+                    </div>
+                    <div class="panel-body">
+
+                        <div class="messages">
+                            <?php
+                                if($errors){
+                                    foreach($errors as $key => $value){
+                                        echo '<div class="alert alert-warning alert-dismissible" role="alert">
+                                                <i class="glyphicon glyphicon-exclamation-sign"></i>'.$value.'</div>';
+                                    }
+                                }
+                            ?>
+                        </div>
+
+                        <form class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" id="">
+                            <div class="form-group">
+                                <label for="inputEmail3" class="col-sm-2 control-label">Username</label>
+                                <div class="col-sm-10">
+                                <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="col-sm-2 control-label">Password</label>
+                                <div class="col-sm-10">
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn btn-default"> <i class="glyphicon glyphicon-log-in"></i> Sign in</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
 
         </div>
