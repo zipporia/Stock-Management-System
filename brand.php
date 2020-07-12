4000537deb99b9413ce503d3c177126618aa3a77
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-12 19:08:19 +0800

    manage brand

---
+++ brand.php
@@ -0,0 +1,27 @@
+<?php require_once 'includes/header.php'; ?>
+
+<div class="row">
+    <div class="col-md-12">
+        <ol class="breadcrumb">
+            <li><a href="dashboard.php">Home</a></li>
+            <li class="active">Brand</li>
+        </ol>
+
+        <div class="panel panel-default">
+            <div class="panel-heading"> <i class="glyphicon glyphicon-edit"></i> Manage Brand</div>
+            <div class="panel-body">
+                <div class="remove-messages"></div>
+
+                <div class="div-action">
+                    <button class="btn btn-default"> <i class="glyphicon glyphicon-plus-sign"></i> Add Brand</button>
+                </div> <!-- /div-action -->
+            </div>
+        </div>
+
+    </div> <!-- /col-md-12 -->
+</div> <!-- /row -->
+
+
+
+
+<?php require_once 'includes/footer.php'; ?>
