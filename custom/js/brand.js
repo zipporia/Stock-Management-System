7d1fef4b4f0bc7206fcc8a6e4d1a920a32542046
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-13 20:20:35 +0800

    ajax

--- custom/js/brand.js
+++ custom/js/brand.js
@@ -21,6 +21,38 @@ $(document).ready(function(){
             $('#brandName').closest('.form-group').addClass('has-success');
         }
 
+        if(brandStatus == ""){
+            $("#brandStatus").after('<p class="text-danger">Brand Status field is required</p>');
+            $("#brandStatus").closest('.form-group').addClass('has-error');
+        }else{
+            // remove error text field
+            $('#brandStatus').find('.text-danger').remove();
+            $('#brandStatus').closest('.form-group').addClass('has-success');
+        }
+
+        if(brandName && brandStatus){
+            var form = $(this);
+
+            // button loading
+            $('#createBrandBtn').button('loading');
+
+            $.ajax({
+                url: form.attr('action'),
+                type: form.attr('method'),
+                data: form.serialize(),
+                dataType: 'json',
+                success: function(response){
+                    // button loading
+                    $("#createBrandBtn").button('reset');
+
+                    if(response.success = true){
+                        // reload the manage member table
+                        manageBrandTAble.ajax.reload(null, false);
+                    }
+                }
+            });
+        }
+
         return false;
     });
 
