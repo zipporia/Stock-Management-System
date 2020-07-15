c635f79ae34e71960532a5a86ee051cbebdcca19
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-15 22:05:23 +0800

    submit brand form to edit brand

--- custom/js/brand.js
+++ custom/js/brand.js
@@ -98,6 +98,78 @@ function editBrands(brandId = null){
             success: function(response){
                 $("#editBrandName").val(response.brand_name);
                 $("#editBrandStatus").val(response.brand_active);
+
+                 // submit brand form function
+                $("#submitBrandForm").unbind('submit').on('submit', function(){
+
+                    // remove the error text
+                    $(".text-danger").remove();
+                    // remove the form error
+                    $(".form-group").removeClass("has-error").removeClass('has-success');
+                    
+                    var brandName = $('#brandName').val();
+                    var brandStatus =$('#brandStatus').val();
+
+                    if(brandName == ""){
+                        $("#brandName").after('<p class="text-danger">Brand Name field is required</p>');
+                        $("#brandName").closest('.form-group').addClass('has-error');
+                    }else{
+                        // remove error text field
+                        $('#brandName').find('.text-danger').remove();
+                        $('#brandName').closest('.form-group').addClass('has-success');
+                    }
+
+                    if(brandStatus == ""){
+                        $("#brandStatus").after('<p class="text-danger">Brand Status field is required</p>');
+                        $("#brandStatus").closest('.form-group').addClass('has-error');
+                    }else{
+                        // remove error text field
+                        $('#brandStatus').find('.text-danger').remove();
+                        $('#brandStatus').closest('.form-group').addClass('has-success');
+                    }
+
+                    if(brandName && brandStatus){
+                        var form = $(this);
+
+                        // button loading
+                        $('#createBrandBtn').button('loading');
+
+                        $.ajax({
+                            url: form.attr('action'),
+                            type: form.attr('method'),
+                            data: form.serialize(),
+                            dataType: 'json',
+                            success: function(response){
+                                // button loading
+                                $("#createBrandBtn").button('reset');
+
+                                if(response.success == true){
+                                    // reload the manage member table
+                                    manageBrandTable.ajax.reload(null, false);
+                                    //reset the form text
+                                    $("#submitBrandForm")[0].reset();
+                                    //remove the error text
+                                    $(".text-danger").remove();
+                                    // remove the form error
+                                    $(".form-group").removeClass('has-error').removeClass('has-success');
+
+                                    $("#add-brand-messages").html('<div class="alert alert-success">'+  
+                                        '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+                                        '<strong> <i class="glyphicon glyphicon-ok-sign"></i> </strong> '+ response.messages +
+                                    '</div>');
+
+                                    $(".alert-success").delay(500).show(10, function(){
+                                        $(this).delay(3000).hide(10, function(){
+                                            $(this).remove();
+                                        });
+                                    });// /.alert
+                                } // 
+                            }
+                        });
+                    }
+                    return false;
+                }); // submit brand form function
+
             } // success
         }); // ajax
     }
