e02f024ddc3a0b30d6a85088c7fb86b01025f0b3
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-17 20:05:55 +0800

    manage datatable

---
+++ custom/js/categories.js
@@ -0,0 +1,10 @@
+var manageCategoriesTable;
+
+$(document).ready(function(){
+    // active top navbar categories
+    $("#navCategories").addClass('active');
+
+    manageCategoriesTable = $('#manageCategoriesTable').DataTable(); // manage categories datatable
+
+
+});
