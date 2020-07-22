7526d20e22367385ba4cf4e6d8827be08a274f78
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-22 21:33:39 +0800

    manage product table

--- product.php
+++ product.php
@@ -10,7 +10,27 @@
         <div class="panel panel-default">
             <div class="panel-heading"><i class="glyphicon glyphicon-edit"></i> Manage Product </div>
             <div class="panel-body">
-                Panel content
+                <div class="remove-messages"></div>
+
+                <div class="div-action pull pull-right" style="padding-bottom: 20px;">
+                    <button class="btn - btn-default"> <i class="glyphicon glyphicon-plus-sign"></i> Add Product</button>
+                </div>
+
+                <table class="table" id="manageProductTable">
+                    <thead>
+                        <tr>
+                            <th style="width:10%;">Photos</th>
+                            <th>Product Name</th>
+                            <th>Rate</th>
+                            <th>Quantity</th>
+                            <th>Brand</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th style="width:15%;">Option</th>
+                        </tr>
+                    </thead>
+                </table>
+
             </div>
         </div>
     </div>
