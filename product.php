202ffaa57b6a91c8110e95751935b73a40bc68eb
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-22 22:03:38 +0800

    Add product form

--- product.php
+++ product.php
@@ -13,7 +13,7 @@
                 <div class="remove-messages"></div>
 
                 <div class="div-action pull pull-right" style="padding-bottom: 20px;">
-                    <button class="btn - btn-default"> <i class="glyphicon glyphicon-plus-sign"></i> Add Product</button>
+                    <button class="btn - btn-default" data-toggle="modal" data-target="#addProductModal"> <i class="glyphicon glyphicon-plus-sign"></i> Add Product</button>
                 </div>
 
                 <table class="table" id="manageProductTable">
@@ -41,17 +41,37 @@
 <div class="modal fade" tabindex="-1" role="dialog" id="addProductModal">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-      <div class="modal-header">
+       
+      <div class="modal-header"> <!-- modal Header-->
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title">Modal title</h4>
+        <h4 class="modal-title"> <i class="fa fa-plus"></i> Add Product </h4>
       </div>
-      <div class="modal-body">
-        <p>One fine body&hellip;</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+        
+    <form class="form-horizontal"><!-- Modal Form-->
+        
+        <div class="modal-body"><!-- modal Body-->
+        
+            <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+            </div>
+        </div>
+            
+        <div class="modal-footer"><!-- modal Footer-->
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+        </div>
+
+    </form>
+
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
