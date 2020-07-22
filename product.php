e5bddc1eb9bb08a0b605a8d2b7fae3f29873fd91
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-22 21:19:13 +0800

    product panel

---
+++ product.php
@@ -0,0 +1,19 @@
+<?php require_once 'includes/header.php'; ?>
+
+<div class="row">
+    <div class="col-mid-12">
+        <ol class="breadcrumb">
+            <li><a href="dashboard.php">Home</a></li>
+            <li class="active">Product</li>
+        </ol>
+
+        <div class="panel panel-default">
+            <div class="panel-heading"><i class="glyphicon glyphicon-edit"></i> Manage Product </div>
+            <div class="panel-body">
+                Panel content
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once 'includes/footer.php'; ?>
