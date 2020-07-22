ec967a3c68387ccad2b658a97ae275353af23e3a
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-22 22:21:33 +0800

    Add product form

--- product.php
+++ product.php
@@ -52,15 +52,45 @@
         <div class="modal-body"><!-- modal Body-->
         
             <div class="form-group">
-                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-                <div class="col-sm-10">
+                <label for="" class="col-sm-3 control-label">Product Image : </label>
+                <div class="col-sm-9">
                 <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
                 </div>
             </div>
             <div class="form-group">
-                <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-                <div class="col-sm-10">
-                <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                <label for="" class="col-sm-3 control-label">Product name : </label>
+                <div class="col-sm-9">
+                <input type="password" class="form-control" id="" name="" placeholder="">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="" class="col-sm-3 control-label">Quality : </label>
+                <div class="col-sm-9">
+                <input type="password" class="form-control" id="" name="" placeholder="">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="" class="col-sm-3 control-label">Rate : </label>
+                <div class="col-sm-9">
+                <input type="password" class="form-control" id="" name="" placeholder="">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="" class="col-sm-3 control-label">Brand Name : </label>
+                <div class="col-sm-9">
+                <input type="password" class="form-control" id="" name="" placeholder="">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="" class="col-sm-3 control-label">Category name : </label>
+                <div class="col-sm-9">
+                <input type="password" class="form-control" id="" name="" placeholder="">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="" class="col-sm-3 control-label">Status : </label>
+                <div class="col-sm-9">
+                <input type="password" class="form-control" id="" name="" placeholder="">
                 </div>
             </div>
         </div>
