b949b06fce5072b99d030d124fa13a89cb58fa9a
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-24 15:44:11 +0800

    product form error

--- custom/js/product.js
+++ custom/js/product.js
@@ -26,5 +26,76 @@ $(document).ready(function(){
             layoutTemplates: {main2: '{preview} {remove} {browse}'},
             allowedFileExtensions: ["jpg", "png", "gif"]
         });
+
+        // submit product form
+        $("#submitProductForm").unbind('submit').bind('submit', function(){
+            var productImage = $("#productImage").val();
+            var productName = $("#productName").val();
+            var quantity = $("#quantity").val();
+            var rate = $("#rate").val();
+            var brandName = $("#brandName").val();
+            var categoryName = $("#categoryName").val();
+            var productStatus = $("#productStatus").val();
+
+            if(productImage == ""){
+                $("#productImage").closest('.center-block').after('<p class="text-danger">The Product Image field is required</p>');
+                $("#productImage").closest('.form-group').addClass('has-error');
+            } else{
+                $("#productImage").find('.text-danger').remove();
+                $("#productImage").closest('.form-group').addClass('has-success');
+            }
+
+            if(productName == ""){
+                $("#productName").after('<p class="text-danger">The Product Name field is required</p>');
+                $("#productName").closest('.form-group').addClass('has-error');
+            } else{
+                $("#productName").find('.text-danger').remove();
+                $("#productName").closest('.form-group').addClass('has-success');
+            }
+
+            if(quantity == ""){
+                $("#quantity").after('<p class="text-danger">The Quantity field is required</p>');
+                $("#quantity").closest('.form-group').addClass('has-error');
+            } else{
+                $("#quantity").find('.text-danger').remove();
+                $("#quantity").closest('.form-group').addClass('has-success');
+            }
+
+            if(rate == ""){
+                $("#rate").after('<p class="text-danger">The Rate field is required</p>');
+                $("#rate").closest('.form-group').addClass('has-error');
+            } else{
+                $("#rate").find('.text-danger').remove();
+                $("#rate").closest('.form-group').addClass('has-success');
+            }
+
+            if(brandName == ""){
+                $("#brandName").after('<p class="text-danger">The Brand Name field is required</p>');
+                $("#brandName").closest('.form-group').addClass('has-error');
+            } else{
+                $("#brandName").find('.text-danger').remove();
+                $("#brandName").closest('.form-group').addClass('has-success');
+            }
+
+            if(categoryName == ""){
+                $("#categoryName").after('<p class="text-danger">The Category field is required</p>');
+                $("#categoryName").closest('.form-group').addClass('has-error');
+            } else{
+                $("#categoryName").find('.text-danger').remove();
+                $("#categoryName").closest('.form-group').addClass('has-success');
+            }
+
+            if(productStatus == ""){
+                $("#productStatus").after('<p class="text-danger">The Status field is required</p>');
+                $("#productStatus").closest('.form-group').addClass('has-error');
+            } else{
+                $("#productStatus").find('.text-danger').remove();
+                $("#productStatus").closest('.form-group').addClass('has-success');
+            }
+
+            
+
+            return false;
+        }); // submit product form
     }); // add product modal btn clicked
 });
